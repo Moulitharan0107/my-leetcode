d3e1fc4e20feed8646faf6ff01d71a6c1d44b68c
Moulitharan IR <moulitharan0107@gmail.com>
2026-08-06 15:10:49 +0000

    Sync LeetCode submission - Find Pivot Index (java)

---
+++ problems/find_pivot_index/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum=0;
+        int l_sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+        }
+        for(int i=0;i<nums.length;i++){
+            if(l_sum==(sum-(l_sum)-nums[i])){
+                return i;
+            }
+            l_sum=l_sum+nums[i];
+        }
+        return -1;
+    }
+}
