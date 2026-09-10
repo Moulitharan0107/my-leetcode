d5ba2141e9dcbd49fe0fe710c2dceb554ebf830f
Moulitharan IR <moulitharan0107@gmail.com>
2026-09-10 21:52:01 +0530

    Time: 4 ms (100.00%) | Memory: 45.8 MB (71.55%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp=x;
+        int rev=0;
+        if(temp<0)
+        {
+            return false;
+        }
+        while(temp!=0)
+        {
+            int digit=temp%10;
+            rev=(rev*10)+digit;
+            temp=temp/10;
+        }
+        if(rev==x)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
